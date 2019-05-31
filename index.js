72afc29ab2eac43bef2741cd3ae571ff23e205f9
Dan Kagan <dankagan7@gmail.com>
2019-05-31 15:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //newPerson in line is fed into function
-function takeANumber(katzDeli, newPerson) {
-  katzDeli.push(newPerson);
+var i = 0;
+// katzdeli = []
+function takeANumber(katzDeli) {
 
 //interpolated message that welcomes the newPerson
-  var welcomeMessage =  `Welcome, ` + newPerson + `. You are number ` + katzDeli.length + ` in line.`;
-
+//i = i + 1
+    katzDeli.push(++i);
+      return `Welcome, you are number ` + i + ` in line.`;
 //returns value welcomeMessage 
-    return welcomeMessage;
+
 }
 
-//////
+
+////// katzDeli = [1, 2, 3, 4];
 
 function nowServing(katzDeliLine) {
   var emptyLine = "There is nobody waiting to be served!";
@@ -21,7 +24,10 @@ function nowServing(katzDeliLine) {
   else { 
     var nextUp = katzDeliLine[0];
     katzDeliLine.shift();
-    return `Currently serving ` + nextUp + `.`;
+    return `Currently serving ` + nextUp + `.`; 
+
+//// return  `Currently serving ${katzDeliLine.shift()}.`; 
+/// katzdeli = [2, 3, 4];
   }
 }
 
@@ -30,7 +36,7 @@ function nowServing(katzDeliLine) {
 // returns the string value for an empty line 
 
 function currentLine(deliLine) {
-  var line = "";
+  var line = [];
   var welcome = `The line is currently: `;
     if (deliLine.length === 0) {
       return "The line is currently empty.";
@@ -39,13 +45,14 @@ function currentLine(deliLine) {
  // line variable reinitiatlizes the return value at the end of the loop and incremenets ontop of it each time through 
  else {
     for (var i = 0; i < deliLine.length; i++) {
-      line = line + (i + 1) + `. ` + deliLine[i];
-     
+      line.push(`${i+1}. ${deliLine[i]}`);
+    ////line = ["1. 2"];
+    ////line = ["1. 2", "2. 3", "3. 4"];
 // this makes sure there are no extra names selected (e.g. undefined)     
       if (i < (deliLine.length - 1))  {
         line = line + `, `;
       } 
-    }
-    return (welcome + line);
+    }//// 
+    return welcome + line.join(', ');
   }  
 }  
